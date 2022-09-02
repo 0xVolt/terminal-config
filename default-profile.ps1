3ab002556824fb412918c3cdf7cdf525d523b178
Desh Iyer <desh.iyer@gmail.com>
2022-09-02 23:42:35 +0530

    Added config terminal script

---
+++ default-profile.ps1
@@ -0,0 +1,14 @@
+# This script runs every time I open my terminal
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme microverse-power
+
+# Other themes that I really enjoy
+# Set-PoshPrompt -Theme ys
+# Set-PoshPrompt -Theme Paradox
+# Set-PoshPrompt -Theme plague
+# Set-PoshPrompt -Theme agnoster
+
+# For more information on oh-my-posh
+# https://ohmyposh.dev/docs/themes/
+# or type 'Get-PoshThemes' in pwrline
